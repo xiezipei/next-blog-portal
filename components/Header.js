deda1e9ad6f9fbb1ec6d1f5c9af424483a90776e
xiezipei <zipeixie@gmail.com>
2021-06-09 17:16:19 +0800

    update: 添加 Header 组件

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/Header.module.css'
+
+const Header = () => (
+    <div className={styles.header}>
+        <h1>Header</h1>
+    </div>
+)
+
+
+export default Header
